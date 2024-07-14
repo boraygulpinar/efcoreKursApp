40034ccc301b5549fab527c366c917dfcf364793
BorayGulpinar <boray112@gmail.com>
2024-07-14 20:12:15 +0300

    commit Kurs CRUD Operations

---
+++ efcoreApp/Controllers/KursController.cs
@@ -0,0 +1,120 @@
+﻿using efcoreApp.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace efcoreApp.Controllers
+{
+    public class KursController : Controller
+    {
+        private readonly DataContext _dataContext;
+        public KursController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var kurslar = await _dataContext.Kurslar.ToListAsync();
+            return View(kurslar);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public async Task<IActionResult> Create(Kurs model)
+        {
+            _dataContext.Kurslar.Add(model);
+            await _dataContext.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var kurs = await _dataContext.Kurslar.FindAsync(id);
+
+            if (kurs == null)
+            {
+                return NotFound();
+            }
+
+            return View(kurs);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Kurs model)
+        {
+            if (id != model.KursId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _dataContext.Update(model);
+                    await _dataContext.SaveChangesAsync();
+                }
+                catch (DbUpdateException)
+                {
+                    if (!_dataContext.Kurslar.Any(k => k.KursId == model.KursId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var kurs = await _dataContext.Kurslar.FindAsync(id);
+
+            if (kurs == null)
+            {
+                return NotFound();
+            }
+
+            return View(kurs);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete([FromForm] int id)
+        {
+            var kurs = await _dataContext.Kurslar.FindAsync(id);
+            if (kurs == null)
+            {
+                return NotFound();
+            }
+            _dataContext.Kurslar.Remove(kurs);
+            await _dataContext.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+    }
+}
